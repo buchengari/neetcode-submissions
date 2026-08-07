e4c7f5c27d0c95da27de36861b0f27c83fe08005
Anudeep Buchengari <adp1724@gmail.com>
2026-08-07 13:08:28 -0400

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        encodedString = ''
+
+        for s in strs:
+            encodedString += f"#{len(s)}#{s}"
+
+        return encodedString
+
+    def decode(self, s: str) -> List[str]:
+        print(s)
+        
+        result = []
+
+        i = 0
+
+        while i < len(s):
+            number = ''
+            if s[i] == '#':
+                # number = ''
+                i += 1
+                try:
+                    while s[i] != '#':
+                        number += s[i]
+                        i += 1        
+                    result.append(s[i+1:i+1+int(number)])
+                except IndexError as e:
+                    continue
+
+            i += int(number) + 1
+
+        return result
+            
+
+        
